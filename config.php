1540a2ca678215c3f65fbc1cf4c44ed5b54571fc
rtanderson2 <rtanderson2@gmail.com>
2014-08-29 11:32:28 -0800

    theme name

--- config.php
+++ config.php
@@ -28,7 +28,7 @@
  * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
-$THEME->name = 'clean';
+$THEME->name = 'BentwoodAK';
 
 /////////////////////////////////
 // The only thing you need to change in this file when copying it to
